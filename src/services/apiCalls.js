a3f3715363c310baea2d543941d4dd63645088f1
Dave86dev <davidochando1986@gmail.com>
2024-03-28 19:27:52 +0100

    feat: login realizado procedentemente con backend local

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
-const API_URL = "https://rickandmortyapi.com/api"
-// const API_URL = "hhtp://localhost:4000"
+// const API_URL = "https://rickandmortyapi.com/api"
+const API_URL = "http://localhost:4000/api/";
 // const API_URL = "aquí hay una url para un entorno de desarrollo"
 
-export const registerNewUserCall = async () => {
+export const registerNewUserCall = async () => {};
 
-}
-
-export const loginCall = async () => {
-
-}
+export const loginCall = async (credentials) => {
+  return await axios.post(`${API_URL}auth/login`, credentials);
+};
 
 // export const bringAllCharacters = async () => {
 //     const res = await axios.get("https://rickandmortyapi.com/api/character", /*headers*/)
@@ -19,18 +17,17 @@ export const loginCall = async () => {
 // }
 
 export const bringAllCharacters = async () => {
-    const res = await axios.get(`${API_URL}/character`, /*headers*/)
+  const res = await axios.get(`${API_URL}/character` /*headers*/);
 
-    return res.data.results
-}
+  return res.data.results;
+};
 
 export const bringCharacterById = async (id) => {
-    // puedo preparar la información para enviar al servidor
-    const res = await axios.get(`${API_URL}/character/${id}`)
+  // puedo preparar la información para enviar al servidor
+  const res = await axios.get(`${API_URL}/character/${id}`);
 
-    return res.data
-}
-
+  return res.data;
+};
 
 // .get("url", {headers}(opcional))
 // .post("url", {body}, {headers})
